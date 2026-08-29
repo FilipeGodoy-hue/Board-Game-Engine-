7bb905c210c26aafea172a1a308cd3f886119093
Filipe Godoy <luis.godoy@upe.br>
2026-08-29 15:41:56 -0300

    testando commit pelo terminal

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Board Game Engine — primeira codagem");
+        System.out.println("Teste Commit 004");
     }
 }
 
