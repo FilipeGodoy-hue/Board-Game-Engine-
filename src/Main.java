aae922543568853aa2e677fbe51b20cfb2ee9582
Filipe Godoy <luis.godoy@upe.br>
2026-09-02 09:24:40 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Board Game Engine — primeira codagem");
         System.out.println("Teste Commit 004");
+        System.out.println("");
     }
 }
 
