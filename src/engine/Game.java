6224aefa820783d5e7e0a218ee9d0d9fd3d179e5
Filipe Godoy <luis.godoy@upe.br>
2026-09-10 15:45:04 -0300

    criando a classe game

---
+++ src/engine/Game.java
@@ -0,0 +1,13 @@
+package engine;
+
+import board.Board;
+import board.Position;
+import java.util.Scanner;
+import player.Player;
+import exceptions.InvalidMoveException;
+import exceptions.InvalidPositionException;
+
+
+public class Game
+{
+}
