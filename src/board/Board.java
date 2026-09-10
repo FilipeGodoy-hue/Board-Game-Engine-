ffaed8491b98d6e11056e446228d78827a856a5a
Filipe Godoy <luis.godoy@upe.br>
2026-09-10 14:25:47 -0300

    implementando a classe board junto com os métodos de posição e também coletando/marcando tudo

---
+++ src/board/Board.java
@@ -0,0 +1,41 @@
+package board;
+
+public class Board {
+
+        private static final char EMPTY = '-';
+
+        private char[][] grid;
+        private int rows;
+        private int columns;
+
+        public Board(int rows , int columns) {
+            this.rows = rows;
+            this.columns = columns;
+
+
+            grid = new char[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+
+                    grid[i][j] = EMPTY;
+                }
+            }
+
+        }
+        public boolean isValidPosition(Position pos){
+        return pos.getRow() >= 0 && pos.getRow() < rows &&pos.getColumn() >= 0 && pos.getColumn() < columns;
+        }
+        public boolean isEmpty(Position pos){
+        if (grid[pos.getRow()][pos.getColumn()] == EMPTY) {
+            return true;
+        }
+        return false;
+        }
+        public void placePiece(Position pos , char symbol){
+            grid[pos.getRow()][pos.getColumn()] = symbol;
+        }
+        public char getPiece(Position pos){
+            return grid[pos.getRow()][pos.getColumn()];
+        }
+}
