5ec7690caf3000d9f4fa2e1a95c697abe878fac0
Filipe Godoy <luis.godoy@upe.br>
2026-09-10 15:21:11 -0300

    testando o main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import board.Board;
+import board.Position;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Board Game Engine — primeira codagem");
         System.out.println("Teste Commit 004");
-        System.out.println("");
     }
 }
 
