c9b16d504b746702b121dba59ca48d92451b5399
Filipe Godoy <luis.godoy@upe.br>
2026-09-15 14:17:31 -0300

    fazendo os atributos e o construtor da classe Game

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -8,6 +8,20 @@ import exceptions.InvalidMoveException;
 import exceptions.InvalidPositionException;
 
 
-public class Game
-{
+public class Game {
+    Board board;
+    Player[] players;
+    int currentPlayerIndex;
+    Scanner scanner = new Scanner(System.in);
+
+    public Game(Player player1 , Player player2) {
+        this.board = new Board(3 ,3);
+        this.players = new Player[2];
+        this.players[0] = player1;
+        this.players[1] = player2;
+        this.currentPlayerIndex = 0;
+        this.scanner = new Scanner(System.in);
+    }
+
+
 }
