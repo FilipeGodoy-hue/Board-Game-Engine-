29d11bd919a7cb8b415c792ff04a4b40cbe62dc7
Filipe Godoy <luis.godoy@upe.br>
2026-09-15 14:52:12 -0300

    implementando condição de vitória no Board

--- src/board/Board.java
+++ src/board/Board.java
@@ -56,4 +56,24 @@ public class Board {
             }
 
         }
+        public boolean checkWinner(char symbol){
+            for (int i = 0; i < rows; i++){
+                if(grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol){
+                    return true;
+                }
+
+            }
+            for (int j = 0; j < columns; j++){
+                if(grid[0][j] == symbol && grid[1][j] == symbol && grid[2][j] == symbol){
+                    return true;
+                }
+            }
+            if(grid[2][0] == symbol && grid[1][1] == symbol && grid[0][2] == symbol){
+                return true;
+            }
+            if(grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol){
+                return true;
+            }
+            return false;
+        }
 }
