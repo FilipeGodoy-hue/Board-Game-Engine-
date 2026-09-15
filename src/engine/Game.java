e4b6fc4534c2004241e1e70988f1113ef531b4c5
Filipe Godoy <luis.godoy@upe.br>
2026-09-15 14:38:32 -0300

    Codando mais

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -14,8 +14,8 @@ public class Game {
     int currentPlayerIndex;
     Scanner scanner = new Scanner(System.in);
 
-    public Game(Player player1 , Player player2) {
-        this.board = new Board(3 ,3);
+    public Game(Player player1, Player player2) {
+        this.board = new Board(3, 3);
         this.players = new Player[2];
         this.players[0] = player1;
         this.players[1] = player2;
@@ -27,15 +27,25 @@ public class Game {
         return players[currentPlayerIndex];
     }
 
-    private void switchTurn(){
+    private void switchTurn() {
 
-        if (currentPlayerIndex == 0){
+        if (currentPlayerIndex == 0) {
             currentPlayerIndex = 1;
-        }
-        else{
+        } else {
             currentPlayerIndex = 0;
         }
 
+    }
+
+    private Position readPosition() {
+
+        System.out.println("Enter the row: ");
+        int row = scanner.nextInt();
+
+        System.out.println("Enter the column: ");
+        int column = scanner.nextInt();
+
+        return new Position(row -1 , column -1);
     }
 
 }
