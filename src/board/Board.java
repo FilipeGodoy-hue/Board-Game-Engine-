4656c32ba18b379dbd0202b127a775bd154954f6
Sofia Ruas <sofiarmmarques@gmail.com>
2026-09-19 19:09:21 -0300

    Board: mensagens de erro mais claras para posição inválida e casa ocupada

--- src/board/Board.java
+++ src/board/Board.java
@@ -37,10 +37,10 @@ public class Board {
         }
         public void placePiece(Position pos , char symbol) throws InvalidMoveException, InvalidPositionException{
             if(!isValidPosition(pos)){
-                throw new InvalidPositionException("Invalid position" + pos.getRow() + "," + pos);
+                throw new InvalidPositionException("Invalid position (" + (pos.getRow() + 1) + "," + (pos.getColumn() + 1) + ") Use row 1-3 and columns 1-3");
             }
             if(!isEmpty(pos)){
-                throw new InvalidMoveException("position held" + pos.getRow() + "," + pos);
+                throw new InvalidMoveException("Position (" + (pos.getRow() + 1) + "," + (pos.getColumn() + 1) + ") is already taken. Choose another one." );
             }
             grid[pos.getRow()][pos.getColumn()] = symbol;
         }
